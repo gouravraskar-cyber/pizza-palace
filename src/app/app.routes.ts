db6d5a07bff0f755e4bbac4863ba8f1fb76a30d2
Gourav Raskar <gouravraskar@gmail.com>
2026-01-28 13:31:23 +0530

    authgaurd added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,24 @@
 import { Routes } from '@angular/router';
+import { authGuard, guestGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
+    canActivate: [authGuard]
   },
   {
     path: 'cart',
-    loadComponent: () => import('./pages/cart/cart.component').then(m => m.CartComponent)
+    loadComponent: () => import('./pages/cart/cart.component').then(m => m.CartComponent),
+    canActivate: [authGuard]
   },
   {
     path: 'auth',
-    loadComponent: () => import('./pages/auth/auth.component').then(m => m.AuthComponent)
+    loadComponent: () => import('./pages/auth/auth.component').then(m => m.AuthComponent),
+    canActivate: [guestGuard]
   },
   {
     path: '**',
-    redirectTo: ''
+    redirectTo: 'auth'
   }
 ];
